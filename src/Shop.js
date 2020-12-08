96901b2dc31030381d274c7a8c79e7921011c09f
FairFightKing <sosthenefruchard@gmail.com>
2020-12-08 14:03:31 +0000

    using data.map and component to render the cards

--- src/Shop.js
+++ src/Shop.js
@@ -1,16 +1,21 @@
 import React, {Component} from 'react';
 import suitComponent from "./Components/shopSuitComponent"
+import data from "./Database/data"
+
 function Shop() {
   return (
     <div className="App">
       <header></header>
         <section>
             <div className="filter">
-                <suitComponent>
-                    
-                </suitComponent>
             </div>
             <div className="content">
+                {data.map((data) => {
+                    return (
+                        <suitComponent img={data.img} name={data.name} price={data.price} color={data.color}>
+                        </suitComponent>
+                    )
+                })}
 
             </div>
         </section>
