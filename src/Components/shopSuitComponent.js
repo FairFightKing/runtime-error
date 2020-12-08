fec47bcbffe3a55a32c9e932130125e89337588d
Julien QUENNEHEN <julien.quennehen@edu.devinci.fr>
2020-12-08 15:33:21 +0000

    fix image logique function shop

--- src/Components/shopSuitComponent.js
+++ src/Components/shopSuitComponent.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const SuitComponent = (props) => {
-    return  (
-        <div data-color={props.color}>
-            <img src={props.img} alt=""/>
-            <div>
-                <h3>{props.name}</h3>
-                <p>price : {props.price}</p>
-                <button>Add to cart</button>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div data-color={props.color}>
+      <img src={props.img} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>price : {props.price}</p>
+        <button>Add to cart</button>
+      </div>
+    </div>
+  );
+};
 
 export default SuitComponent;
--- src/Shop.js
+++ src/Shop.js
@@ -1,33 +1,37 @@
-import React, {Component} from 'react';
-import SuitComponent from "./Components/shopSuitComponent"
-
+import React from "react";
+import SuitComponent from "./Components/shopSuitComponent";
+import image from "./img/image.png";
 function Shop() {
-    const data = [
-        {
-            "name" : "Adidas",
-            "price" : 80,
-            "color" : "black",
-            "img" : "url"
-        }
-    ]
+  const data = [
+    {
+      name: "Adidas",
+      price: 80,
+      color: "black",
+      img: "url",
+      image: image,
+    },
+  ];
   return (
     <div className="App">
-      <header>
-
-      </header>
-        <section>
-            <div className="filter">
-            </div>
-            <div className="content">
-                {data.map((e, i) => {
-                    return <SuitComponent img={e.img} name={e.name} price={e.price} color={e.color} key={i}/>
-                })}
-
-            </div>
-        </section>
+      <header></header>
+      <section>
+        <div className="filter"></div>
+        <div className="content">
+          {data.map((e, i) => {
+            return (
+              <SuitComponent
+                img={e.image}
+                name={e.name}
+                price={e.price}
+                color={e.color}
+                key={i}
+              />
+            );
+          })}
+        </div>
+      </section>
     </div>
   );
 }
-
 
 export default Shop;
