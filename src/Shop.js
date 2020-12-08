60c65b68057f92becf4383a9f21575b9c746db49
tirdys <antoine.duplouy@edu.devinci.fr>
2020-12-08 16:28:53 +0100

    modify template

--- src/Shop.js
+++ src/Shop.js
@@ -1,9 +1,6 @@
 import React, {Component} from 'react';
 import SuitComponent from "./Components/shopSuitComponent"
-<<<<<<< HEAD
-import data from "./Database/data"
-=======
->>>>>>> da5ae40d0d426e718ca737467d1c76bc5492e424
+
 
 function Shop() {
     const data = [
